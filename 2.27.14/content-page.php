b6748b5e58598cfa62dcb367ced175ce85fb98bd
Christopher Stein <prof.stein@gmail.com>
2014-02-27 14:00:49 -0500

    Added files for 2.27.14

---
+++ 2.27.14/content-page.php
@@ -0,0 +1,13 @@
+<!-- 
+This is where you add the HTML for how you want the content of your page to appear.
+Some template tags you might want to use:
+
+	<?php the_title(); ?>
+	Filed in: <?php the_category(', '); ?>
+	<?php the_tags('Taged with: ', ', ') ?>
+	<?php the_post_thumbnail('feature-image' ); ?> Change feature-image to whatever name you defined for the featured image
+	<?php the_content(); ?>
+
+Add your content below. Delete this if you like 
+-->
+
